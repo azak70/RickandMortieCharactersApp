a12be261961fc95c460dd45c2fe0eac4d2ffa4ac
Ahmet AZAK <azakahmet@gmail.com>
2023-08-03 06:40:51 +0300

    Initial Commit

---
+++ RickyMortyAppSwiftWithoutSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyMortyAppSwiftWithoutSB
+//
+//  Created by ahmet azak on 3.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
